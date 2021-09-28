16bcfeb9e3d0196ffd1c7a08e275d7cde106d528
= <mohsenghalandar1998>
2021-09-28 10:58:14 +0330

    finish header and user input <no responsive

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function add()
+{
+    const title = document.getElementById("title").value;
+    const ex1 = document.getElementById("ex1").value;
+    const ex2 = document.getElementById("ex2").value;
+
+
+}
